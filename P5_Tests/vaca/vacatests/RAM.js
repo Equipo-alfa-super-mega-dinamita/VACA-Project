6f2f9fbd8d4519092cac0d5379562e20b38efaee
Nicolai Romero <anromerom@unal.edu.co>
2020-02-01 00:23:46 -0500

    Cambiado estilo de componentes Memory y Registers

--- P5_Tests/vaca/vacatests/RAM.js
+++ P5_Tests/vaca/vacatests/RAM.js
@@ -1,4 +1,4 @@
-const nitems = 8;
+const nitems = 10;
 const toHex = (i, pad) => (i).toString(16).padStart(pad,'0')
 const toBin = (i, pad) => (i).toString(2).padStart(pad,'0');
 class RAM {
@@ -8,6 +8,7 @@ class RAM {
         this.x = x;
         this.y = y;
         this.size = size;
+        this.inHex = true;
         this.gui = createGui();
         this.buffer = new ArrayBuffer(array_size);
         this.memory = new Array(array_size);
@@ -15,23 +16,38 @@ class RAM {
         let n = this.memory.length;
         for (let i = 0; i <  n; i++) {
             this.memory[i] = {
-                address:   toHex(i,4),
+                address:  toHex(i,4),
                 color: color('#111111')
             }
             this.memoryContent[i] = floor(random(2**8));
         }
-        this.slider = createSliderV("RAM-Slider", x+ size + 2, y, 0.1*size, 2*size, 0, array_size - nitems*2)
+
+        this.slider = createSliderV("RAM-Slider", x+ size + 2 + size*0.02, y, 0.1*size , 2.2*size , 2, array_size - nitems*2);
         this.slider.isInteger = true
         this.slider.setStyle({
             rounding: 0,
             strokeWeight: 0,
-            trackWidth: 0.5
+            trackWidth: 0.1,
+
+            fillBg: color('rgba(255,255,255,0)'),
+            fillBgActive: color('rgba(255,255,255,0)'),
+            fillBgHover: color('rgba(255,255,255,0)'),
+
+            fillHandle: color('#58b368'),
+            fillTrack: color('#275108'),
+
+            fillHandleHover: color('#b3ff49'),
+            fillTrackHover: color('#3f6038'),
+
+            fillTrackActive: color('#aaaaaa'),
+            fillHandleActive: color('#e7e7e7')
 
         });
 
 
         //setInterval(()=>{    this.slider.val+=1}, 1000)
         console.log(this.memory)
+        console.log(this.memoryContent)
 
     }
 
@@ -39,31 +55,33 @@ class RAM {
 
     display (){
         push();
-        noStroke(0);colorMode(HSB,100); textSize(this.size/20);
+        noStroke(0);colorMode(HSB,100);
+
         let start = floor(this.slider.val) ;
-        start -= start%2;
         let h = 2*this.size / nitems;
 
-        let max = nitems > this.memory.length - start ? (this.memory.length - start): nitems*2;
+        let max = nitems;
 
-        for(let j = 0; j< max; j+=2){
-            let index = start + (max - j - 2);
+        for(let j = 0; j< max; j+=1){
+            let index = start + 2*j;
             //  H Register
-            this.memorySlot(this.x, this.y + j*h/2 , this.size, index);
+            //console.log({index});
+            this.memorySlot(this.x, this.y + (max- j-1)*(h + this.size*0.02) , this.size, index-2);
+            this.memorySlot(this.x + this.size/2 +this.size*0.02 , this.y + (max-j-1)*(h + this.size*0.02) , this.size, index -1);
+
             //  L Register
-            this.memorySlot(this.x + this.size/2, this.y + j*h/2, this.size, index + 1);
 
-            push();stroke(0); strokeWeight(3); fill(255);
+            push();stroke(0); strokeWeight(2); fill(255);textSize(this.size/25);
 
-            text(this.memory[index].address,
+            //console.log(this.memory[index].address)
+            text(this.memory[index-2].address,
                  this.x - this.size*0.15,
-                this.y + j*h*0.5 + h*0.75,
+                this.y + (max-j-1)*h + h*0.75,
                 this.size*0.15,
-                h*0.25); pop();
+                h*0.25);
+            pop();
         }
-
         fill(0); stroke(0);
-        text(this.slider.val, this.x + this.size*1.2 ,this.y + this.size/2);
         pop();
     }
 
@@ -71,33 +89,59 @@ class RAM {
 
     memorySlot(x,y,size, index){
         push();
-        stroke('#07da63'); strokeWeight(2); fill(this.memory[index].color);
 
+        //fill(this.memory[index] !== undefined ? this.memory[index].color : '#ffffff');
+        strokeWeight(size*0.01);
         let h = 2 * size / nitems;
-
+        stroke('#007065');
+        noFill();
         rect(x,
             y ,
             size/2,
             h);
 
-        textAlign(CENTER, CENTER); textSize(size/22 );
+
+        textAlign(CENTER, CENTER); textSize(size/20 );
 
         let bin = toBin(this.memoryContent[index], 8).split('');
+        rectMode(CORNER);
+        fill('#007065');
+        rect(x,y, size*0.15, h*0.25);
+        rect(
+            x,y + h*0.7,
+            size/2, 0.3*h
+        );
+        rect(
+            x,y,
+            size*0.5,
+        h*0.05);
+
+        rect(
+            x,y,
+            0.05*h, h
+        );
+        rect(
+            x + size*0.5 ,y,
+            -0.05*h, h
+        );
         for (let i = 0; i < 8 ; i++) {
-            stroke('#07da63');
+            stroke('#007065');
             line(2 + x + i * (size-8) / 16,
-                -2 + y + 0.95 * h,
+                -2 + y + 0.9 * h,
                 2 + x  + i * (size-8) / 16,
                 -2 + y + h);
-
-            fill( bin[i] === '0' ? '#ffaaaa' : '#8888ff'); stroke(0); strokeWeight(2);
+            let back = color(BACKGROUND_COLOR);
+            if( bin[i] === '0' ){
+                back.setAlpha(55);
+            }
+            fill( back );
+            noStroke();textSize(size*0.045);
             text(
                 bin[i],
                 x + i * (size) / 16,
-                y + h*0.75,
+                y + h*0.7,
                 (size) / 16,
-                0.25*h);
-
+                0.3*h);
         }
 
         line(2 + x ,
@@ -109,36 +153,42 @@ class RAM {
             2 + x  + 8 * (size-8) / 16,
             -2 + y + 0.9 * h);
 
-        push();textSize(size/18 ); fill('#07da63'); stroke(0); strokeWeight(2);
+        textSize(size*0.04 ); fill(BACKGROUND_COLOR); noStroke();
         text(
-            this.memoryContent[index],
+            '0x' + this.memory[index].address,
             x,
             y ,
-            size/4,
-            h*0.75
+            size*0.16,
+            h*0.25
         );
-
+        textSize(size*0.08 );   fill('#58b368');
 
         text(
-            '0x' + toHex(this.memoryContent[index],2),
-            x +size/4,
+            this.inHex ? '0x' + toHex(this.memoryContent[index],2) : this.memoryContent[index],
+            x ,
             y ,
-            size/4,
+            size*0.5,
             h*0.75
         );
-        pop();
+
         pop();
     }
 
 
     scroll(x,y,delta){
 
+
         let d =-delta
         if(x > this.x && x < this.x + this.size && y > this.y && y < this.size*2 ){
 
-            if(Math.sign(d) < 0 && this.slider.val <= 0) return;
-            if(Math.sign(d) > 0 && this.slider.val + nitems*2>= this.memory.length - 1) return;
-            this.slider.val += Math.sign(d)*nitems/2
+            let newval = floor(this.slider.val + Math.sign(d)*nitems);
+
+            newval-= newval % 2;
+
+            newval = newval < 0 ? 2: newval;
+            newval = newval > this.memoryContent.length - 2 * (nitems-1) ? this.memoryContent.length - 2*(nitems-1): newval;
+            this.slider.val = newval;
+
 
         }
 
--- P5_Tests/vaca/vacatests/Registers.js
+++ P5_Tests/vaca/vacatests/Registers.js
@@ -3,123 +3,233 @@ class Registers{
     constructor(x,y,size) {
         this.x = x;
         this.y = y;
-        this.size = size
+        this.size = size;
+        this.inHex = true;
+        colorMode(HSB,100);
         this.flags = {
             control:{
                 'TF': {
                     'name': 'Trap Flag',
-                    'description': ''
+                    'description': '',
+                    id: 'TF',
+                    x: this.x + 0.05*size,
+                    y: this.y + 1.5 *size,
+                    col: '#e08f62',
+                    value: false
                 },
                 'DF':{
                     'name': 'Direction Flag',
-                    'description': ''
+                    'description': '',
+                    id: 'DF',
+                    x: this.x + 0.235*size,
+                    y: this.y + 1.5 *size,
+                    col: '#d7c79e',
+                    value: false
                 },
                 'IF':{
                     'name': 'Interrupt Flag',
-                    'description': ''
+                    'description': '',
+                    id: 'IF',
+                    x: this.x + 0.42*size,
+                    y: this.y + 1.5 *size,
+                    col: '#9dab86',
+                    value: false
                 }
             },
             status:{
                 'OF':{
                     'name': 'Overflow Flag',
-                    'description': ''
+                    'description': '',
+                    id: 'OF',
+                    x: this.x + 0.05*size,
+                    y: this.y + 1.75 *size,
+                    col: '#1f4287',
+                    value: false
                 },
                 'SF':{
                     'name': 'Sign Flag',
-                    'description': ''
+                    'description': '',
+                    id: 'SF',
+                    x: this.x + 0.235*size,
+                    y: this.y + 1.75 *size,
+                    col: '#278ea5',
+                    value: false
                 },
                 'ZF':{
                     'name': 'Zero Flag',
-                    'description': ''
+                    'description': '',
+                    id: 'ZF',
+                    x: this.x + 0.42*size,
+                    y: this.y + 1.75 *size,
+                    col: '#21e6c1',
+                    value: false
                 },
                 'AF':{
                     'name': 'Auxiliary Carry Flag',
-                    'description': ''
+                    'description': '',
+                    id: 'AF',
+                    x: this.x + 0.7*size,
+                    y: this.y + 1.75 *size,
+                    col: '#c05c7e',
+                    value: false
                 },
                 'PF':{
                     'name': 'Parity Flag',
-                    'description': ''
+                    'description': '',
+                    id: 'PF',
+                    x: this.x + 0.885*size,
+                    y: this.y + 1.75 *size,
+                    col: '#f3826f',
+                    value: false
                 },
                 'CF':{
                     'name': 'Carry Flag',
-                    'description': ''
+                    'description': '',
+                    id: 'CF',
+                    x: this.x + 1.07*size,
+                    y: this.y + 1.75 *size,
+                    col: '#ffb961',
+                    value: false
                 }
                 
             }
             
-        }
+        };
+
         this.registers =
             {
                 general: {
                     'AX': {
+                        'id': 'AX',
                         'name': 'Accumulator Register',
-                        'description': ''
+                        'description': '',
+                        x: this.x + this.size*0.7,
+                        y: this.y + this.size*0.05,
+                        col:'#fd5e53',
+						content: Math.floor(random(2**8))
                     },
                     'BX': {
+                        'id': 'BX',
                         'name': 'Base Register',
-                        'description': ''
+                        'description': '',
+                        x: this.x + this.size*0.7,
+                        y: this.y + this.size*0.27,
+                        col:'#75b79e',
+						content: Math.floor(random(2**8))
                     },
                     'CX': {
+                        'id': 'CX',
                         'name': 'Count Register',
-                        'description': ''
+                        'description': '',
+                        x: this.x + this.size*0.7,
+                        y: this.y + this.size*0.49,
+                        col:'#9aceff',
+						content: Math.floor(random(2**8))
                     },
                     'DX': {
+                        'id': 'DX',
                         'name': 'Data Register',
-                        'description': ''
+                        'description': '',
+                        x: this.x + this.size*0.7,
+                        y: this.y + this.size*0.71,
+                        col:'#fdd365',
+						content: Math.floor(random(2**8))
                     },
                 },
                 pointer:{
                     'SP': {
+                        'id': 'SP',
                         'name': 'Stack Pointer',
-                        'description': ''
+                        'description': '',
+                        x: this.x + this.size*0.7,
+                        y: this.y + this.size*1.0,
+                        col:'#12cad6',
+						content: Math.floor(random(2**8))
                     },
                     'BP': {
+                        'id': 'BP',
                         'name': 'Base Pointer',
-                        'description': ''
-                    },
+                        'description': '',
+                        x: this.x + this.size*0.7,
+                        y: this.y + this.size*1.22,
+                        col:'#ccda46',
+						content: Math.floor(random(2**8))
+                    }
+                },
+                index:{
                     'SI': {
+                        'id': 'SI',
                         'name': 'Source Index',
-                        'description': ''
+                        'description': '',
+                        x: this.x + this.size*0.05,
+                        y: this.y + this.size*1.0,
+                        col:'#93b5b3',
+						content: Math.floor(random(2**8))
                     },
                     'DI': {
+                        'id': 'DI',
                         'name': 'Destination Index',
-                        'description': ''
+                        'description': '',
+                        x: this.x + this.size*0.05,
+                        y: this.y + this.size*1.22,
+                        col:'#95a7a0',
+						content: Math.floor(random(2**8))
                     }
                 },
                 segment:{
                     'CS': {
+                        'id': 'CS',
                         'name': 'Code Segment',
-                        'description': ''
+                        'description': '',
+                        x: this.x + this.size*0.05,
+                        y: this.y + this.size*0.05,
+                        col:'#3fc5f0',
+						content: Math.floor(random(2**8))
                     },
                     'DS': {
+                        'id': 'DS',
                         'name': 'Data Segment',
-                        'description': ''
+                        'description': '',
+                        x: this.x + this.size*0.05,
+                        y: this.y + this.size*0.27,
+                        col:'#42dee1',
+						content: Math.floor(random(2**8))
                     },
                     'SS': {
+                        'id': 'SS',
                         'name': 'Stack Segment',
-                        'description': ''
+                        'description': '',
+                        x: this.x + this.size*0.05,
+                        y: this.y + this.size*0.49,
+                        col:'#6decb9',
+						content: Math.floor(random(2**8))
                     },
                     'ES': {
+                        'id': 'ES',
                         'name': 'Extra Segment',
-                        'description': ''
+                        'description': '',
+                        x: this.x + this.size*0.05,
+                        y: this.y + this.size*0.71,
+                        col:'#a1dd70',
+						content: Math.floor(random(2**8))
                     }
                 },
                 instruction:{
                     'IP': {
+                        'id': 'IP',
                         'name': 'Instruction Pointer',
-                        'description': ''
+                        'description': '',
+                        x: this.x + this.size*0.7,
+                        y: this.y + this.size*1.5,
+                        col:'#ca5fa6',
+						content: Math.floor(random(2**8))
                     }
                 }
-            }
+            };
 
-        let data = { ...this.flags, ... this.registers}
+        let data = { ...this.flags, ... this.registers};
         console.log(data)
-        for (let type in data){
-            for(let item in data[type]){
-                console.log(data[type][item].name)
-            }
 
-        }
 
 
 
@@ -127,57 +237,232 @@ class Registers{
 
 
     display() {
-
+        push();colorMode(HSB);
         //REGISTERS
+        stroke(255);
+        noFill();
+        rect(this.x, this.y, this.size*1.25, this.size * 2);
         noStroke();
-        fill(255);
-        rect(this.x, this.y, this.size, this.size * 0.8);
+
+        for (let item in this.registers['general']){
+            this.registerDisplay( this.registers['general'][item]);
+        }
+        for (let item in this.registers['instruction']){
+            this.registerDisplay( this.registers['instruction'][item]);
+        }
+
+        for (let item in this.registers['segment']){
+            this.registerDisplay( this.registers['segment'][item]);
+        }
+        for (let item in this.registers['pointer']){
+            this.registerDisplay( this.registers['pointer'][item]);
+        }
+
+        for (let item in this.registers['index']){
+            this.registerDisplay( this.registers['index'][item]);
+        }
+
+        for (let item in this.flags['control']){
+            this.flagDisplay(this.flags['control'][item], this.size*0.125)
+        }
+        for (let item in this.flags['status']){
+            this.flagDisplay(this.flags['status'][item], this.size*0.125)
+        }
 
 
-        stroke(0);
+        pop();
+    }
 
-        rect(this.x + this.size*0.05,
-            this.y + this.size*0.05,
-            this.size*0.9,
-            this.size*0.35);
-        rect(
-            this.x + this.size*0.05,
-            this.y + this.size * 0.4,
-            this.size*0.9,
-            this.size*0.35
+    flagDisplay({x,y,col,id, value},size){
+        push();
+        noStroke(); fill( 51);
+        //rect(x,y,size,size*1.58);
+        strokeWeight(size*0.08);
+        noFill(); stroke(value ? col : 'rgba(136,136,136,0.75)');
+        ellipseMode(RADIUS);
+        textAlign(CENTER, CENTER)
+        ellipse(x + size*0.5,
+                y + size* 0.79,
+                size*0.6,
+                size*0.6   );
+        noStroke(); fill(value ? col : 'rgba(136,136,136,0.75)');
+        textSize(size* 0.65);
+        //stroke(0);
 
-        )
+        text(id,
+          x - size*0.23,
+            y ,
+            size*1.5,
+            size*1.4
+        );
+        pop();
+    }
 
-        line( this.x + this.size*0.6,
-              this.y + this.size*0.4,
-              this.x + this.size*0.6,
-              this.y + this.size*0.75,
-
-
-        )
-
-        //Current Register
-        fill(51,25,60);
-        rect(this.x + this.size*0.1,
-            this.y + this.size*0.1,
-            this.size* 0.5,
-            this.size*0.25);
-
-        ellipse(
-            this.x + this.size*0.65,
-            this.y + this.size*0.1,
-            this.size*0.25,
-            this.size*0.25
-        )
+    registerDisplay({x,y,col,id,content}) {
+        //console.log(color)
 
 
+        if (content === undefined) return;
+        id = id ? id : '';
+        push();
+        noStroke();
+
+        let size = this.size * 0.5;
+
+        textSize(size * 0.18);
+        noFill();
+        stroke(col);
+        strokeWeight(size * 0.02);
+        strokeJoin(ROUND);
+        let h = size * 0.4;
+        rect(x, y, size, h);
+        const toBin = (i, pad) => (i).toString(2).padStart(pad, '0');
+        let bin = toBin(content, 8).split('');
+        fill(col);
+        rect(x, y + h*0.7, size,  h * 0.3);
+        for (let i = 0; i < 8; i++) {
+
+            line(x + i * size / 8,
+                y + size * 0.34,
+                x + i * size / 8,
+                y + size * 0.4);
+            push();
+
+            let back = color(BACKGROUND_COLOR);
+            if (bin[i] === '0') {
+                back.setAlpha(55);
+            }
+            fill(back);
+            textSize(size * 0.09);
+            textAlign(CENTER, CENTER);
 
 
+
+            noStroke();
+            text(
+                bin[i],
+                x + i * (size) / 8,
+                y + h * 0.7,
+                (size) / 8,
+                0.3 * h);
+
+            pop();
+        }
+        noStroke();
+
+        fill(col);
+
+        textAlign(CENTER, CENTER);
+        if(id === 'AX' || id === 'BX' || id === 'CX' || id === 'DX')
+        {
+            textSize(size * 0.16);
+
+            stroke(col);
+            line(x + size/2,
+                    y + h/2,
+                x+ size/2,
+                y + h
+                );
+            push();
+            rectMode(RADIUS);
+            rect(x + size * 0.5,
+                y + h*0.35,size * 0.15, h*0.35
+                );
+            pop();
+            fill(BACKGROUND_COLOR);
+            text(id,
+                x,
+                y - size * 0.02,
+                size,
+                size * 0.17);
+
+            fill(col); textSize(size * 0.12); noStroke();
+            text(id[0] + 'H',
+                x,
+                y - size * 0.02,
+                size/3,
+                size * 0.17);
+            text(id[0] + 'L',
+                x+ size/2 + size/6,
+                y - size * 0.02,
+                size/3,
+                size * 0.17);
+            textSize(size * 0.10);
+
+
+            let hcontent, lcontent;
+
+
+
+            hcontent = parseInt(bin.slice(0,4).join(''), 2)
+            lcontent = parseInt(bin.slice(4,8).join(''), 2)
+
+            text(lcontent,
+                x+ size/2 + size/6,
+                y + size * 0.1,
+                size/3,
+                size * 0.17);
+
+
+            text(hcontent,
+                x,
+                y + size * 0.1,
+                size/3,
+                size * 0.17);
+
+            fill(BACKGROUND_COLOR);
+            rectMode(CENTER);
+            text(content,
+                x + size/2,
+                y + size * 0.19,
+                size/3,
+                size * 0.17);
+
+
+
+
+
+        }
+        else
+        {
+            rect(x,
+                y,
+            size * 0.35,
+            size * 0.3);
+            rect(x + size * 0.35,
+                y,
+                size * 0.65,
+                size * 0.02);
+            rect(x + size,
+                y,
+                -size * 0.02,
+                size * 0.3);
+
+            textSize(size * 0.18);
+            fill(BACKGROUND_COLOR);
+            text(id,
+                x + size * 0.02,
+                y,
+                size * 0.3,
+                size * 0.25);
+            fill(col);
+            textSize(size * 0.14);
+
+            text(this.inHex ? '0x' + content.toString(16).padStart('0', 2) : content,
+                x + size * 0.35,
+                y,
+                size * 0.65,
+                size * 0.3
+            );
+            content.toString(16).padStart('0', 2);
+
+
+        }
+        pop();
 
 
 
     }
-
 
 
 
