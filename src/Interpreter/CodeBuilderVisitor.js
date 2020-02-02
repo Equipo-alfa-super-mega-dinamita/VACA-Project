b4b1f2be1c2907b3782eb99f3b0dfbc4972a31cc
Edder <edhernandezfo@unal.edu.co>
2020-02-02 11:54:07 -0500

    Var to let

--- src/Interpreter/CodeBuilderVisitor.js
+++ src/Interpreter/CodeBuilderVisitor.js
@@ -18,7 +18,7 @@ class CodeBuilderVisitor extends asm8086Visitor.asm8086Visitor{
     }
 
     start(ctx){
-        var res = this.visitProg(ctx);
+        let res = this.visitProg(ctx);
         //console.log(this.ramContent);
         //console.log(this.labelContents);
         return {labelContents: this.labelContents,ramContent:this.ramContent,memoryPos :this.memoryPos,codeArray: this.codeArray};
@@ -31,7 +31,7 @@ class CodeBuilderVisitor extends asm8086Visitor.asm8086Visitor{
         if(ctx.instruction()){
             //Guardar par etiqueta/contexto
             if(ctx.lbl()){
-                var labelText = ctx.lbl().label().getText();
+                let labelText = ctx.lbl().label().getText();
                 //Existe la columna?
                 if(!this.labelContents.hasOwnProperty(labelText)){
                     this.labelContents[labelText] = this.memoryPos;
@@ -48,12 +48,12 @@ class CodeBuilderVisitor extends asm8086Visitor.asm8086Visitor{
     }
 
     visitInstruction(ctx){
-        var codeObj = {};
+        let codeObj = {};
         //console.log(ctx.start.line); //OBTENER LINEA Y COLUMNA DEL CTX para generar errores
         //todo errores de cantidad de argumentos
-        var opStr = ctx.opcode().getText();
-        var argsStr = "";
-        var args;
+        let opStr = ctx.opcode().getText();
+        let argsStr = "";
+        let args;
 
         if (ctx.expressionlist()) {
             args = this.visitExpressionList(ctx.expressionlist());
@@ -71,7 +71,7 @@ class CodeBuilderVisitor extends asm8086Visitor.asm8086Visitor{
 
     visitExpressionList(ctx) {
         //console.log(ctx.expression(null));
-        var args = [];
+        let args = [];
         let expressions = ctx.expression(null);
         expressions.forEach((expr) => {
             args.push(this.visitExpression(expr));
@@ -80,13 +80,13 @@ class CodeBuilderVisitor extends asm8086Visitor.asm8086Visitor{
     }
 
     visitExpression(ctx){
-        var firstVal = this.visitMultiplyingExpression(ctx.multiplyingExpression(0));
+        let firstVal = this.visitMultiplyingExpression(ctx.multiplyingExpression(0));
         let multiplyingExpressions = ctx.multiplyingExpression(null).length;
         if(multiplyingExpressions>1){
             //Para sumar las expresiones y regresar el valor numerico total (implementar getRawVal para que sirva)
             //Las sumas no se hacen en la construccion de codigo, sino en ejecución
-            var returnValue = {type:"sum", value:{operands:[],signs:""}};
-            for(var iter = 0;iter<multiplyingExpressions-1;iter++){
+            let returnValue = {type:"sum", value:{operands:[],signs:""}};
+            for(let iter = 0;iter<multiplyingExpressions-1;iter++){
                 returnValue.value.signs+=ctx.SIGN(iter).getText();
                 returnValue.value.operands.push(this.visitMultiplyingExpression(ctx.multiplyingExpression(iter)));
                 /*if(ctx.SIGN(iter-1).getText() === "-"){
@@ -110,7 +110,7 @@ class CodeBuilderVisitor extends asm8086Visitor.asm8086Visitor{
     }
 
     visitArgument(ctx){
-        var argObj = {};
+        let argObj = {};
         if(ctx.number()){
             argObj.type = "number";
             argObj.value = parseInt(ctx.number().getText());
@@ -133,10 +133,10 @@ class CodeBuilderVisitor extends asm8086Visitor.asm8086Visitor{
             argObj.type = "ptr";
             argObj.value = ctx.ptr().getText();
         }else{
-            var txt = ctx.getText();
-            var checkChar = txt.charAt(txt.length-1);
+            let txt = ctx.getText();
+            let checkChar = txt.charAt(txt.length-1);
             if(checkChar===']'){
-                var exprContent = this.visitExpression(ctx.expression());
+                let exprContent = this.visitExpression(ctx.expression());
                 argObj.type = "memory";
                 argObj.value = exprContent.value;
                 argObj.valueType = exprContent.type;
